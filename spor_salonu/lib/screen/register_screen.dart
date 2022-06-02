a6c10aa175d01af052503d1aef2b649df0e19d8b
xwork1 <osman_120_444@hotmail.com>
2022-06-03 00:29:02 +0300

    update

--- spor_salonu/lib/screen/register_screen.dart
+++ spor_salonu/lib/screen/register_screen.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:spor_salonu/model/user_model.dart';
-import 'package:spor_salonu/model/vucut_endeks_model.dart';
 
 import '../constants.dart';
 
